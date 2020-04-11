a78aac0657ebf90041062a5b70a45ba2a9ddeba6
VladimirOrlovA <45207216+VladimirOrlovA@users.noreply.github.com>
2020-04-11 15:32:25 +0600

    Add project files.

---
+++ ADO.NET_hw_lesson03/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ADO.NET_hw_lesson03.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADO.NET_hw_lesson03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string _ConnStr;
+        static SqlConnection _Connection;
+        static Frame _MainFrame;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ConnStr = ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString;
+            _Connection = new SqlConnection(_ConnStr);
+            _MainFrame = frMainFrame;
+        }
+
+        private void btnTablesEquipment_Click(object sender, RoutedEventArgs e)
+        {
+            string command = "SELECT TOP(10) * FROM TablesEquipment";
+            _MainFrame.Navigate(new PageTableView(command, _Connection, "TablesEquipment"));
+        }
+
+        private void btnTablesManufacturer_Click(object sender, RoutedEventArgs e)
+        {
+            string command = "SELECT TOP(10) * FROM TablesManufacturer";
+            _MainFrame.Navigate(new PageTableView(command, _Connection, "TablesManufacturer"));
+        }
+
+        private void btnTablesStopReason_Click(object sender, RoutedEventArgs e)
+        {
+            string command = "SELECT TOP(10) * FROM TablesStopReason";
+            _MainFrame.Navigate(new PageTableView(command, _Connection, "TablesStopReason"));
+        }
+    }
+}
---
+++ ADO.NET_hw_lesson03/Pages/PageTableView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADO.NET_hw_lesson03.Pages
+{
+    /// <summary>
+    /// Interaction logic for PageTableView.xaml
+    /// </summary>
+    public partial class PageTableView : Page
+    {
+        private SqlDataAdapter dataAdapter;
+        private DataSet dataSet;
+        private DataTableMapping tableMapping;
+
+        public PageTableView(string command, SqlConnection connection, string tableName)
+        {
+            InitializeComponent();
+            dataAdapter = new SqlDataAdapter(command, connection);
+            tableMapping = dataAdapter.TableMappings.Add("Table", tableName);
+            FillTable(tableName);
+        }
+
+        private void FillTable(string tableName)
+        {
+            dataSet = new DataSet(tableName);
+            dataAdapter.Fill(dataSet);
+            dataSet.Tables[0].TableName = tableName;
+            dgTable.ItemsSource = dataSet.Tables[tableName].DefaultView;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            dataAdapter.Update(dataSet);
+        }
+
+        private void dgTable_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            btnSave.IsEnabled = true;
+        }
+    }
+}
